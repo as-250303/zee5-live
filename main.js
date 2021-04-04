2730d01666ecda0cdf96af923b57aff23416e937
as-250303 <81795019+as-250303@users.noreply.github.com>
2021-04-04 20:59:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
   function bttt(){
     let int=document.querySelector("input");
 
-    let newurl="http://127.0.0.1:5500/ZEE5/stream.html"+"?url="+ encodeURIComponent(int.value);
+    let newurl="https://as-250303.github.io/zee5-live/stream.html"+"?url="+ encodeURIComponent(int.value);
     console.log(newurl);
       window.location.assign(newurl);
   }
